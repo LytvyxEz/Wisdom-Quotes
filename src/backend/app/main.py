bf22f8e5023f70c0102ac6277121fa8fd787b7e0
LytvyxEz <maxlytw@gmail.com>
2025-11-06 17:23:34 +0200

    changed app.

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -5,14 +5,14 @@ from fastapi.staticfiles import StaticFiles
 from db import lifespan
 from api import quoutes_router, home_router
 
-def init_app():
-    
-    app = FastAPI(title='Winsdom Quotes', lifespan=lifespan)
+class App:
+    def __call__(self, *args, **kwds):
+        app = FastAPI(title='Winsdom Quotes', lifespan=lifespan)
 
-    app.mount("/static", StaticFiles(directory="src/frontend/static"), name="static")
-    app.include_router(quoutes_router)
-    app.include_router(home_router)
-    
-    return app
+        app.mount("/static", StaticFiles(directory="src/frontend/static"), name="static")
+        app.include_router(quoutes_router)
+        app.include_router(home_router)
+        
+        return app
 
-app = init_app()
+app = App()
